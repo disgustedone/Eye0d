ebb8a87e6adca399e058bf44ffda8324bd3b28f2
disgustedone <58005436+disgustedone@users.noreply.github.com>
2020-10-17 10:39:38 +0200

    Update ports-scaner.py

--- ports-scaner.py
+++ ports-scaner.py
@@ -1,8 +1,8 @@
 import socket
 host = input('please enter you ip address: ')
 try:
-  for port in range(1.1000):
-    s = socket.sockets(socket.AF_INET, socket.SOCK_STREAM)
+  for port in range(1,2000):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket.setdefaulttimeout(1)
     result = s.connect_ex((host,port))
     if result == 0:
