428ef7c530bc1cf9acd18a95d71f9eef123781b7
disgustedone <58005436+disgustedone@users.noreply.github.com>
2020-10-17 10:07:18 +0200

    Create ports-scaner.py

---
+++ ports-scaner.py
@@ -0,0 +1,11 @@
+import socket
+host = input('please enter you ip address: ')
+try 
+  for port in range(1.1000):
+    s = socket.sockets(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(1)
+    result = s.connect_ex((host,port))
+    if result == 0:
+      print("the port {} is open.".format(port))
+except:
+  print('error ')
